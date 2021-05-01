fddc4b418d0f4c119925c505ba2ad0275fee3ede
123 <da_buddha@bk.ru>
2021-05-01 23:31:37 +0300

    fe

--- bot.py
+++ bot.py
@@ -226,7 +226,7 @@ def send3(event: VkBotMessageEvent):
 
 def send6(event: VkBotMessageEvent):
     vk.messages.send(peer_id=event.message.peer_id,
-                     message="Адрес: 191186, г. Санкт-Петербург, наб. реки Мойки, корп. 2, ауд. 266\n\nТелефон: +7 (812) 571-10-03\n\nПочта: icsto@herzen.spb.ru\n\nЧасы приема директора института: вт, чт с 14:00 до 16:00",
+                     message="Адрес: 191186, гСанкт-Петербург, наб. реки Мойки, корп. 2, ауд. 266\n\nТелефон: +7 (812) 571-10-03\n\nПочта: icsto@herzen.spb.ru\n\nЧасы приема директора института: вт, чт с 14:00 до 16:00",
                      random_id=get_random_id())  # ДОБАВИЛ ЯЯЯЯЯЯ
 
 
